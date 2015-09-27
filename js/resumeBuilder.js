6cd898dff3f460868b35bc3c8ac42ab69f888d1a
anydoby <anydoby@gmail.com>
2015-09-27 19:44:31 +0200

    added some javascript objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = HTMLheaderName.replace("%data%",'Sergey Zolotaryov');
+var title = HTMLheaderRole.replace("%data%", 'Tech Lead');
+
+$('#header').append(name).append(title);
+
+var bio = {
+	name: "Sergey Zolotaryov",
+	role: "Tech Lead",
+	email: "anydoby@gmail.com",
+	pictureURL: "images/me.jpg",
+	age: 33,
+	
+};
